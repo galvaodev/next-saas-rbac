7ac87e150fc0a4d6e7e28eb73f5aef02703e8073
galvaopp <galvaopp@gmail.com>
2024-09-28 10:16:42 -0300

    chore: Add tsconfig.json for API project and update package.json in typescript-config

---
+++ apps/api/src/index.ts
@@ -0,0 +1,9 @@
+import { defineAbilityFor } from '@saas/auth'
+const ability = defineAbilityFor({ role: 'MEMBER' })
+
+const userCanInviteSomeoneElese = ability.can('manage', 'User')
+const userCanDeleteOtherUsers = ability.can('delete', 'User')
+const userCannotDeleteOtherUsers = ability.cannot('delete', 'User')
+console.log(userCanInviteSomeoneElese)
+console.log(userCanDeleteOtherUsers)
+console.log(userCannotDeleteOtherUsers)
